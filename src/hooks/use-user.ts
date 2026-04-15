96f281ba61e7b5e11c7dbe10758edbd1e9394751
vishwas <vishwasreddy367@gmail.com>
2026-04-15 17:25:40 +0530

    Secondary commit

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import type { UserContextValue } from '@/contexts/user-context';
 import { UserContext } from '@/contexts/user-context';
-
+ 
 export function useUser(): UserContextValue {
   const context = React.useContext(UserContext);
 
